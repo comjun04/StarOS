ffa77407a01d63b2d45a1f97469a2d399a3d72d9
comjun04 <30339539+comjun04@users.noreply.github.com>
2018-07-13 20:46:35 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+package StarOS;
+
+import StarOS.boot.Boot;
+
+public class Main {
+	
+	public static final String ver = "0.1";
+	public static final String local_ver = "0.1";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			new Boot();
+	}
+
+}
