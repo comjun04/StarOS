9fa1257155eb851731c6cd9cae5634510353d911
comjun04 <30339539+comjun04@users.noreply.github.com>
2019-02-10 20:07:34 +0900

    add comment

--- src/disk_load_rm.asm
+++ src/disk_load_rm.asm
@@ -1,19 +1,22 @@
+; load DH sectors to ES:BX from drive DL
 disk_load:
-	push dx
+	push dx	       ; Store DX on stack so later we can recall
+		       ; how many sectors were request to be read ,
+		       ; even if it is altered in the meantime
 	
-	mov ah, 0x02 ; BIOS read sector function
-	mov al, dh   ; Read DH sectors
-	mov ch, 0x00 ; Select cylinder 0
-	mov dh, 0x00 ; Select head 0
-	mov cl, 0x02 ; start reading from sector 2 (sector 1 = boot sector)
+	mov ah, 0x02   ; BIOS read sector function
+	mov al, dh     ; Read DH sectors
+	mov ch, 0x00   ; Select cylinder 0
+	mov dh, 0x00   ; Select head 0
+	mov cl, 0x02   ; start reading from sector 2 (sector 1 = boot sector)
 	
-	int 0x13
+	int 0x13       ; BIOS interrupt
 	
-	jc disk_error
+	jc disk_error  ; Jump if error ( i.e. carry flag set )
 	
-	pop dx
-	cmp dh, al
-	jne disk_error
+	pop dx	       ; Restore DX from the stack
+	cmp dh, al     ; if AL ( sectors read ) != DH ( sectors expected )
+	jne disk_error ; display error message
 	ret
 
 disk_error:
